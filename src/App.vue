3ee079fad3a0b30c11dfb82fcb9a2ff39393ff50
ekta-rathi <ekta.rathi@oxfordcc.co.uk>
2018-11-22 15:49:55 +0000

    Initial Commit with sass loader

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="World" />
+    <div class="testing">Testing the sample app</div>
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
+
+<style lang="scss">
+/* write SCSS here */
+.testing {
+	color: green
+}
+</style>
---
+++ src/components/HelloComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="message">Hello {{ name }}!</h1>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/* write SCSS here */
+@import '../../assets/scss/main.scss' 
+
+</style>
